0250803a3e63a81314408737f0829f5177a347a9
Molsbee <molsbee@gmail.com>
2018-06-13 08:10:32 -0500

    Worked on basic UI layout.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,21 +2,27 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
-const ReactBootstrap = require('react-bootstrap');
 const restClient = require('./restclient');
+const BrowserRouter = require('react-router-dom');
+// const { Router, Route, IndexRoute, IndexLink, Link } = require('react-router');
+
+// import {ChannelList} from './component/channel-list'
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {userDetails: {}, channels: []};
+        this.state = {
+            userDetails: {},
+            channels: [],
+            channel: ""
+        };
     }
 
     componentDidMount() {
         restClient({method: 'GET', path: '/users?self=true'}).then(response => {
             const userDetails = response.entity[0];
             const channels = userDetails.subscribedChannels ? userDetails.subscribedChannels : [];
-            console.log("Channels: " + channels);
             this.setState({
                 userDetails: userDetails,
                 channels: channels
@@ -25,54 +31,37 @@ class App extends React.Component {
     }
 
     render() {
+        const channels = this.state.channels.map(channel => {
+            return <a className={"nav-link"} onClick={this.handleClick.bind(this, channel.name)}>{channel.name}</a>
+        });
+
         return (
-            <div>
-                <nav className={"navbar navbar-expand navbar-dark bg-dark"}>
+            <div className={"container-fluid h-100"}>
+                <nav className={"navbar navbar-dark fixed-top bg-dark flex-md-nowrap"}>
                     <a className={"navbar-brand"} href={"#"}>
                         Relay<br/>
                         <span style={{fontSize: "14px"}}>{this.state.userDetails.firstName} {this.state.userDetails.lastName}</span>
                     </a>
                 </nav>
-                <div className={"container-fluid"}>
-                    <div className={"row"}>
-                        <div className={"col-md-2"} style={{backgroundColor: "#006680", color: "#f2f2f2"}}>
-                            <div className={"row"}>
-                                {/*User channels*/}
-                                <ChannelList channels={this.state.channels}/>
-                            </div>
+                <div className={"row h-100"}>
+                    <div className={"col-md-3"} style={{backgroundColor: "#006680", color: "#f2f2f2", paddingTop: "90px"}}>
+                        <div id={"channels"}>
+                            <nav className={"nav flex-column"}>Channels</nav>
+                            <nav className={"nav flex-column"}>{channels}</nav>
                         </div>
-                        <div className="col-md-10">
-                            {/*Message Page*/}
+                    </div>
+                    <div className={"col-md-9"} style={{paddingTop: "90px"}}>
+                        <div style={{height: "95%"}}>
+                            {/*<Channel channel={this.state.channel}/>*/}
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Default</span>
+                            </div>
+                            <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
-
-}
-
-class ChannelList extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {channel: ""};
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(channelName) {
-        console.log(channelName);
-    }
-
-    render() {
-        console.log(this.props.channels);
-        const channels = this.props.channels.map(channel => {
-            return <a className={"nav-link"} onClick={this.handleClick.bind(this, channel.name)}>{channel.name}</a>
-        });
-
-        return (
-            <div id="channels">
-                <nav className={"nav flex-column"}>{channels}</nav>
             </div>
         )
     }
